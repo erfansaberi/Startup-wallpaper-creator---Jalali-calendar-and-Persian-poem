9fd91342fea3919ee0e461764a232cc9c768974c
Erfan Saberi <erfansmart12@gmail.com>
2021-01-08 14:00:43 +0330

    prepared for development

--- app.py
+++ app.py
@@ -1,73 +1,94 @@
 import ctypes
-import os
-import urllib.request
 import json
+import os
+import platform
+import random
 import sys
 import time
-import wget
+import urllib.request
 
 import arabic_reshaper
 import jdatetime
 import requests
+import wget
 from bidi.algorithm import get_display
 from PIL import Image, ImageDraw, ImageFont
+
+import config
 
 
 def set_wallpaper(wallpaper_name='\output.jpg'):
     cwd = os.getcwd()
-    print(cwd+wallpaper_name)
-    ctypes.windll.user32.SystemParametersInfoW(20, 0, cwd+wallpaper_name , 0)
+    if platform.system() == 'Windows':
+        ctypes.windll.user32.SystemParametersInfoW(
+            20, 0, cwd+wallpaper_name, 0)
+    elif platform.system() == 'Linux':
+        # I did not tested this line.
+        os.system(
+            f"/usr/bin/gsettings set org.gnome.desktop.background picture-uri {cwd+wallpaper_name}")
+    else:
+        input('Your operating system is not supported')
 
 
-def write_on_image(wallpaper_name='wallpapers/wallpaper.jpg',text='روز خوبی داشته باشید',font='fonts/Sahel.ttf'):
-    fontFile = font
+def write_on_image(wallpaper_name='wallpapers/wallpaper.jpg', poem=config.alternative_poem, font='Sahel.ttf'):
+    # Load font
+    fontFile = f'fonts/{font}'
+    font = ImageFont.truetype(fontFile, config.font_size)
+    # Load Image
     imageFile = wallpaper_name
-    font = ImageFont.truetype(fontFile, 50)
     image = Image.open(imageFile)
-    reshaped_text = arabic_reshaper.reshape(text)
-    bidi_text = get_display(reshaped_text)
-    trans = str.maketrans('1234567890','۱۲۳۴۵۶۷۸۹۰')
-    today = str(jdatetime.datetime.now().date()).translate(trans)
     draw = ImageDraw.Draw(image)
-    w,h = draw.textsize(text=text,font=font)
-    draw.text((image.width-w,image.height-120-h), bidi_text, (255,255,255), font=font)
-    w,h = draw.textsize(text=today,font=font)
-    draw.text((image.width-w-100,image.height-100-h), today, (255,255,255), font=font)
+    # Poem
+    if config.write_poem:
+        reshaped_text = arabic_reshaper.reshape(poem)
+        bidi_text = get_display(reshaped_text)
+        w, h = draw.textsize(text=poem, font=font)
+        draw.text((image.width-w, image.height-120-h),
+                  bidi_text, (255, 255, 255), font=font)
+    # Jalali Datetime
+    if config.write_jalali_datetime:
+        trans = str.maketrans('1234567890', '۱۲۳۴۵۶۷۸۹۰')
+        today = str(jdatetime.datetime.now().date()).translate(trans)
+        w, h = draw.textsize(text=today, font=font)
+        draw.text((image.width-w-100, image.height-100-h),
+                  today, (255, 255, 255), font=font)
     draw = ImageDraw.Draw(image)
     image.save("output.jpg")
 
 
-def get_text():
-    #try:
-    if 1:
-        r = requests.get('http://c.ganjoor.net/beyt-json.php')
-        res = r.json()
-        text = res['m1'] + '\n    ' + res['m2'] + '\n' + res['poet']
-        return text
-    try:
-        pass
-    except Exception as e:
-        return '''
-        عیب رندان مکن ای زاهد پاکزه سرت
-        که گناه دگران بر تو نخواهند نوشت
-        نه من از پرده تقوا به در افتادم و بس
-        پدرم نیز بهشت ابد از دست بهشت
-        '''
+def get_poem():
+    if config.online_random_poem:
+        try:
+            r = requests.get('http://c.ganjoor.net/beyt-json.php')
+            res = r.json()
+            text = res['m1'] + '\n    ' + res['m2'] + '\n' + res['poet']
+            return text
+        except:
+            return config.alternative_poem
+    elif config.offline_random_poem:
+        return ''
+    else:
+        return config.alternative_poem
 
 
 def get_wallpaper():
     try:
-        image_filename = wget.download('https://picsum.photos/1920/1080',out='wallpapers')
-        return image_filename
+        if config.online_random_wallpaper:
+            image_filename = wget.download(
+                'https://picsum.photos/1920/1080', out='wallpapers')
+            return image_filename
+        elif config.offline_random_wallpaper:
+            image_filename = random.choice(
+                os.listdir(os.getcwd()+'\\wallpapers'))
+            return 'wallpapers\\'+image_filename
     except:
         return 'wallpapers/alternative.jpg'
 
 
 def start():
-    text = get_text()
-    today = jdatetime.datetime.now().date
+    poem = get_poem()
     wallpaper = get_wallpaper()
-    write_on_image(text=text,wallpaper_name=wallpaper)
+    write_on_image(poem=poem, wallpaper_name=wallpaper)
     set_wallpaper('\output.jpg')
 
 
---
+++ config.py
@@ -0,0 +1,28 @@
+# Wallpaper configs
+static_wallpaper = False
+online_random_wallpaper = False
+offline_random_wallpaper = True
+alternative_wallpaper = 'alternative.jpg'
+
+## Content
+# text
+write_jalali_datetime = True
+write_gregorian_datetime = False #Will be added soon
+write_poem = True
+write_quran = False #Will be added soon
+write_hafez_divination = False #Will be added soon
+font_size = 50
+default_font = 'Sahel.ttf'
+# poem
+online_random_poem = True
+alternative_poem = '''نه من از پرده تقوا به در افتادم و بس
+پدرم نیز بهشت ابد از دست بهشت'''
+offline_random_poem = False
+# quran
+online_random_aya = True
+alternative_aya = ''
+
+# hafez_divination
+online_random_divination = True
+alternative_divination = ''
+
